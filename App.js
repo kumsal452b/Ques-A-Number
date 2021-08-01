9cbd0e679d8539414935b5a0ba89aa26b6cbc8b4
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-08-01 21:52:51 +0300

    Add new color scheme

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Header from "./components/Header";
 export default function App() {
+  const title = "Merhaba";
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View style>
+        <Header title={title} />
+      </View>
     </View>
   );
 }
@@ -14,8 +16,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    backgroundColor:
+      "radial-gradient(circle, rgba(58,151,221,1) 49%, rgba(24,40,87,1) 66%)",
+    marginTop: 20,
   },
 });
--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,28 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
+import LinearGradient from "react-native-linear-gradient";
 
 const Header = (props) => {
   return (
-    <View>
-      <Text>{props.title}</Text>
+    <View style={styles.header}>
+      <LinearGradient>
+        <Text style={styles.text}>{props.title}</Text>
+      </LinearGradient>
     </View>
   );
 };
-
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor:
+      "radial-gradient(circle, rgba(58,151,221,1) 49%, rgba(24,40,87,1) 66%)",
+    marginTop: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    height: 50,
+  },
+  text: {
+    color: "white",
+    fontSize: 23,
+  },
+});
 export default Header;
