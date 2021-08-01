4cb9af9b08d004916bccbbe2dba7932089a110f9
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-08-01 20:58:24 +0300

    add new header component

--- components/Header.js
+++ components/Header.js
@@ -1,2 +1,12 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
+
+const Header = (props) => {
+  return (
+    <View>
+      <Text>{props.title}</Text>
+    </View>
+  );
+};
+
+export default Header;
