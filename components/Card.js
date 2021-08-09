cbdb8aa7c1ee5ef6dec05096d71681f502cd7e80
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-08-09 13:56:51 +0300

    add new style element to card js

--- components/Card.js
+++ components/Card.js
@@ -1,2 +1,20 @@
 import React from "react";
 import { Text, View, Textinput, Stylesheet } from "react-native";
+
+const Card=(props)=>{
+
+};
+const styles=Stylesheet.create({
+    card:{
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center",
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 0 },
+    shadowRadius: 6,
+    backgroundColor: "white",
+    elevation: 10,
+  },
+});
+export default Card;
