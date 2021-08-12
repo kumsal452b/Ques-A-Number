326723b0059390ce5b6e21943904e7f312be61eb
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-08-12 21:56:33 +0300

    add new acreen element

---
+++ screens/GameScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const generateNumberBetween = (min, max, exclude) => {};
+
+const GameScreen = (props) => {
+  return <View></View>;
+};
+const styles = StyleSheet.create({});
+export default GameScreen;
