4e3c4d49a996e77f6e84fe6a0e4a4943cf58b284
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-08-13 19:23:30 +0300

    Add new touchable element

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,43 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import NumberInputContainer from "../components/NumberInputContainer";
+import Card from "../components/Card";
 
-const generateNumberBetween = (min, max, exclude) => {};
+const generateNumberBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const rndm = Math.floor(Math.random() * (max - min)) + min;
+  if (rndm === exclude) {
+    return generateNumberBetween(min, max, exclude);
+  } else {
+    return rndm;
+  }
+};
 
 const GameScreen = (props) => {
-  return <View></View>;
+  const [currentGues, setCurrentGues] = useState(
+    generateNumberBetween(1, 100, props.userChoice)
+  );
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Gues</Text>
+      <NumberInputContainer>{currentGues}</NumberInputContainer>
+      <Card>
+        <TouchableOpacity>
+          <Text>LOWER</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text>GREATER</Text>
+        </TouchableOpacity>
+      </Card>
+    </View>
+  );
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems: "center",
+    padding: 10,
+  },
+});
 export default GameScreen;
